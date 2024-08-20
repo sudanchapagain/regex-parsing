2895e9b017f5a41fdec62a8143dff40c694b6fb8
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-08-20 18:02:47 +0545

    stage 2

--- app/main.js
+++ app/main.js
@@ -1,38 +1,16 @@
 function matchPattern(inputLine, pattern) {
-  if (pattern.length === 1) {
+  if (pattern === "\\d") {
+    return /[0-9]/.test(inputLine);
+  } else if (pattern.length === 1) {
     return inputLine.includes(pattern);
   } else {
     throw new Error(`Unhandled pattern ${pattern}`);
   }
 }
 
-function checkDigit() {
-  const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-  for (let char of digits) {
-    if (digits.includes(char)) {
-      return true;
-    }
-  }
-  return false;
-}
-
 function main() {
   const pattern = process.argv[3];
   const inputLine = require("fs").readFileSync(0, "utf-8").trim();
-
-  if (process.argv[2] !== "-E") {
-    console.log("Expected first argument to be '-E'");
-    process.exit(1);
-  }
-
-  if (process.argv[4] === "\\d") {
-    if (checkDigit()) {
-      process.exit(1);
-    } else {
-      process.exit(0);
-    }
-  }
 
   if (matchPattern(inputLine, pattern)) {
     process.exit(0);
