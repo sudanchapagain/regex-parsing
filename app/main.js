4a1da16469d963a7b67c423539ef588a93ed1e84
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2024-08-20 19:19:20 +0545

    stage 3

--- app/main.js
+++ app/main.js
@@ -1,11 +1,40 @@
 function matchPattern(inputLine, pattern) {
+  const inputLength = inputLine.length;
+
+  // match digits
   if (pattern === "\\d") {
-    return /[0-9]/.test(inputLine);
+    for (let i = 0; i < inputLength; i++) {
+      const charCode = inputLine.charCodeAt(i);
+      // ASCII codes for '0' to '9'
+      if (charCode >= 48 && charCode <= 57) {
+        return true;
+      }
+    }
+
+    // match alphanumeric character or underscore
+  } else if (pattern === "\\w") {
+    for (let i = 0; i < inputLength; i++) {
+      const charCode = inputLine.charCodeAt(i);
+      if (
+        // 0 to 9
+        (charCode >= 48 && charCode <= 57) ||
+        // A to Z
+        (charCode >= 65 && charCode <= 90) ||
+        // a to z
+        (charCode >= 97 && charCode <= 122) ||
+        // underscore
+        charCode === 95
+      ) {
+        return true;
+      }
+    }
   } else if (pattern.length === 1) {
     return inputLine.includes(pattern);
   } else {
     throw new Error(`Unhandled pattern ${pattern}`);
   }
+
+  return false;
 }
 
 function main() {
